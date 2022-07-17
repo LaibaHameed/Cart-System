c90a71130e694eb2d769cc869cf74e2a3c25f8cb
LaibaHameed <laibahameed012@gmail.com>
2022-07-16 18:43:38 -0700

    add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './main-portal/cart/cart.component';
+import { HomeComponent } from './main-portal/home/home.component';
+import { ProductsComponent } from './main-portal/products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: HomeComponent},
+  {path: "home", component: HomeComponent},
+  {path: "cart", component:CartComponent},
+  {path: "products", component:ProductsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './main-portal/header/header.component';
+import { HomeComponent } from './main-portal/home/home.component';
+import { ProductsComponent } from './main-portal/products/products.component';
+import { CartComponent } from './main-portal/cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    ProductsComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
